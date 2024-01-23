19995189c74363a43a5bd1e2f50f49ffbe4f7503
Noa Kehle <nkehle@calpoly.edu>
2024-01-23 13:02:34 -0800

    added search by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,6 +15,10 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
   };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
   
   app.get("/users", (req, res) => {
     const name = req.query.name;
@@ -24,6 +28,16 @@ const findUserByName = (name) => {
       res.send(result);
     } else {
       res.send(users);
+    }
+  });
+
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
     }
   });
 
